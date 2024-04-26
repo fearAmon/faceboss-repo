48cd26969726ca8a0fcc2f417fcf3625d03538b5
fearAmon <businesswith.tola@gmail.com>
2024-04-26 02:48:45 +0100

    Faceboss

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// SWIPER SLIDER
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+// NAVIGATION BAR EFFECTS ON SCROLL
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+// // RESPONSIVE NAVIGATION MENU TOOGLE
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
